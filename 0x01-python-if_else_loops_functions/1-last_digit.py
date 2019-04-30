9090921492ca14fa657bd2db3cc48b56253976d5
Trill Ali <alishabelle98@gmail.com>
2019-04-30 19:57:18 +0000

    splitting sentences

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_d = number % 10
+if last_d > 5:
+    print("Last digit of {:d}".format(number) + " is " +
+          "{:d} and is greater than 5".format(last_d))
+elif last_digit == 0:
+    print("Last digit of {:d}".format(number) + " is " +
+          "{:d} and is 0".format(last_d))
+else:
+    print("Last digit of {:d}".format(number) + " is " +
+          "{:d} and is less than 6 and not 0".format(last_d))
