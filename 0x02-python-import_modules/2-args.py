cc3d51b387ebbf31cbad14744aa95874f1114d9b
Trill Ali <alishabelle98@gmail.com>
2019-05-02 17:32:26 +0000

    fixed pcs errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 from sys import argv
-argc = len(argv) - 1
-if argc == 0:
-    print("{:d} arguments.".format(argc, end="\n"))
-elif argc == 1:
-    print("{:d} argument:".format(argc, end="\n"))
-else:
-    print("{:d} arguments:".format(argc, end="\n"))
-for i, v in enumerate(argv):
-    if i == 0:
-        continue
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc == 0:
+        print("{:d} arguments.".format(argc, end="\n"))
+    elif argc == 1:
+        print("{:d} argument:".format(argc, end="\n"))
     else:
-        print("{}: {}".format(i, v, end="\n"))
+        print("{:d} arguments:".format(argc, end="\n"))
+        for i, v in enumerate(argv):
+            if i == 0:
+                continue
+            else:
+                print("{}: {}".format(i, v, end="\n"))
