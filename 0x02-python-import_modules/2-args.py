73a469f91f0893e0ff058184b918acbe6d64e6f7
Trill Ali <alishabelle98@gmail.com>
2019-05-02 17:24:12 +0000

    printing a list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+argc = len(argv) - 1
+if argc == 0:
+    print("{:d} arguments.".format(argc, end="\n"))
+elif argc == 1:
+    print("{:d} argument:".format(argc, end="\n"))
+else:
+    print("{:d} arguments:".format(argc, end="\n"))
+for i, v in enumerate(argv):
+    if i == 0:
+        continue
+    else:
+        print("{}: {}".format(i, v, end="\n"))
