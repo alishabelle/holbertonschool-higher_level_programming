ff21f6d923837e3cff8aa3b8bc7e4664afa85fea
Trill Ali <alishabelle98@gmail.com>
2019-05-08 21:47:49 +0000

    finds all multiples of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    new_jawn = []
+    for i in my_list:
+        if i % 2 == 0:
+            new_jawn.append(True)
+        else:
+            new_jawn.append(False)
+    return new_jawn
