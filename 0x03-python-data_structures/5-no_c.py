5022e744672867624b8c5b65f0880d3d3109f716
Trill Ali <alishabelle98@gmail.com>
2019-05-08 17:15:19 +0000

    fixing pcs errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    changer = {99:None, 67:None}
+    changer = {99: None, 67: None}
     new_string = my_string.translate(changer)
     return (new_string)
