a0589dc3089533a8ffcda98a7e5ee5dcd7482754
Trill Ali <alishabelle98@gmail.com>
2019-05-18 04:16:58 +0000

    betty fixes and compiled code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,8 +3,8 @@
 #include "lists.h"
 /**
  *check_cycle - function that cycles through linked list
- *
- *
+ *@list: parameter
+ *Return: returns 0 if no cycle, 1 if cycle
  */
 int check_cycle(listint_t *list)
 {
@@ -16,17 +16,16 @@ int check_cycle(listint_t *list)
 		return (0);
 	if (list->next == NULL)
 		return (0);
-	
+
 	skips = list;
 	node = list;
 
 	while (skips && skips->next && node && node->next)
-	{	
+	{
 		if (skips == node)
 			return (1);
 		skips = skips->next->next;
 		node = node->next;
-	
 	}
 	return (0);
 }
