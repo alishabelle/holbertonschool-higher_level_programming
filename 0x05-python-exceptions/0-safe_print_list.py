7f401f67d5c5419df37428c8cfce99993009472f
Trill Ali <alishabelle98@gmail.com>
2019-05-20 20:07:15 +0000

    first commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
+    for index in range(0, x):
+        try:
+            print("{}".format(my_list[index]), end="")
+            count += 1
+        except IndexError:
+              print()
+              return (count)
+    print()
+    return (count)
