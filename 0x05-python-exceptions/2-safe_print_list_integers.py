c4d05b00646661be9957973f67b4a9bad41bf403
Trill Ali <alishabelle98@gmail.com>
2019-05-21 03:10:52 +0000

    push for checks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
-        for index in range(x):
+        for index in range(0, x):
             if type(my_list[index]) != int:
                 continue
-            print("{}".format(my_list[index]), end="")
+            print("{:d}".format(my_list[index], end="")
             count += 1
         print("")
         return count
