e06875f1e5e819a4bf32b37d44b3ce3bd6d8d519
Trill Ali <alishabelle98@gmail.com>
2019-05-22 17:29:25 +0000

    fixed pcs errors

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,11 +3,10 @@ class Square:
     """defines a square"""
     def __init__(self, size=0):
         self.__size = size
-    
+
     @property
     def size(self):
         return self.__size
-
 
     @size.setter
     def size(self, value):
@@ -16,6 +15,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-           
+
     def area(self):
         return self.__size * self.__size
