47bbd2fe4754736ff745ef869e91c6f951a478c7
Trill Ali <alishabelle98@gmail.com>
2019-05-22 18:36:48 +0000

    push for output

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -20,7 +20,7 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        if self.__size = 0:
+        if self.__size == 0:
             print()
         else:
             for index in range(self.__size):
