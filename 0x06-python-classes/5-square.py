3356ab4b55a89949b87ff5482bb52d06a72fe0cc
Trill Ali <alishabelle98@gmail.com>
2019-05-22 18:12:19 +0000

    fixed for pcs errors

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-class Square:                                                                   
-    """defines a square"""                                                      
-    def __init__(self, size=0):                                                 
-        self.__size = size                                                      
-                                                                                
-    @property                                                                   
-    def size(self):                                                             
-        return self.__size                                                      
-                                                                                
-    @size.setter                                                                
-    def size(self, value):                                                      
-        if not isinstance(value, int):                                          
-            raise TypeError("size must be an integer")                          
-        if value < 0:                                                           
-            raise ValueError("size must be >= 0")                               
-        self.__size = value                                                     
-                                                                                
-    def area(self):                                                             
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
         return self.__size * self.__size
 
     def my_print(self):
-        if self.__size == 0:
+        if self.__size = 0:
             print()
         else:
-            for x in range(self.__size):
+            for index in range(self.__size):
                 print("#" * self.__size)
