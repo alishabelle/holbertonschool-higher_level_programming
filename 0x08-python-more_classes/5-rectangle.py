eedb554ce73e8eb82a7f4b016e19891e0725e97f
Trill Ali <alishabelle98@gmail.com>
2019-05-31 03:40:03 +0000

    fixed output

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 class Rectangle:
     """Defining a rectangle setting width and height"""
+      
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-
+   
     @property
     def width(self):
         return self.__width
