f05d47730732744b06eab88a89322d12e00b6697
Trill Ali <alishabelle98@gmail.com>
2019-06-08 21:27:28 +0000

    push to github

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+""" module documentation - read number of lines """
+
+
 def read_lines(filename="", nb_lines=0):
+    """documentation for reading number of lines"""
+    
+    if nb_lines is <= 0 or nb_lines 
+with open(filename) as f:
+        print(f.read(nb_lines), end="")
--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+import json
+""" documentation module """
+
 def load_from_json_file(filename):
+    """ load json """
+    with open(filename) as f:
+        return json.load(f)
