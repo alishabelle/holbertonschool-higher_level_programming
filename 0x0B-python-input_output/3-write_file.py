3078f561dd9d297dbf87fdad62f8f2c97087770e
Trill Ali <alishabelle98@gmail.com>
2019-06-08 19:30:49 +0000

    push output

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+""" documentation module - to write file"""
+
+
 def write_file(filename="", text=""):
+    """ write file documentation """
+    with open(filename, mode='w') as f:
+        f.write(text)
+        return(len(text))
