bb203085f91c7b9fdd5a835619533da29b07ca9e
Trill Ali <alishabelle98@gmail.com>
2019-06-08 21:16:04 +0000

    correct output

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+import json
+""" documentation module """
+
+
 def save_to_json_file(my_obj, filename):
+    """ write a function that writes """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
