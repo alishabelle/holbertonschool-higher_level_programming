1992650c93fef9ddcf1604ab581e3026b71e42d2
Trill Ali <alishabelle98@gmail.com>
2019-06-08 19:36:28 +0000

    correct output to append file

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+""" documentation module """
+
+
 def append_write(filename="", text=""):
+    """documentation to append file"""
+    with open(filename, mode='a') as f:
+        f.write(text)
+        return(len(text))
