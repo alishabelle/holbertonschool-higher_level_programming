0a91f3b99ed0a8d8a0bea88c673269aae52b4c64
Trill Ali <alishabelle98@gmail.com>
2019-06-08 18:48:44 +0000

    push output

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+""" module documentation - read file lines """
+
+
 def number_of_lines(filename=""):
+    """documentation for reading file lines"""
+    line_number = 0
+    with open(filename) as f:
+        for line in f:
+            line_number += 1
+        return(line_number)
