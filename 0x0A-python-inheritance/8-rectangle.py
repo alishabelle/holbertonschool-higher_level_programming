885c177a856104495945eaf5ec6ec15b2a8c3e56
Trill Ali <alishabelle98@gmail.com>
2019-06-12 18:05:27 +0000

    recommit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """ documentation module - improve geometry """
 
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+class BaseGeometry:
+    """ class that raises an exception """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
-    """ rectangle class that inherits basegeometry class """
+    """ Rectangle class that inherits bsaegeometry class """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
