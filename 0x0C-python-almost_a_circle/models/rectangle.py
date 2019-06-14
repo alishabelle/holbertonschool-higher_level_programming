ba44351ddab3c3c339d72e1afba35f0e8eb441ee
Trill Ali <alishabelle98@gmail.com>
2019-06-14 09:27:28 +0000

    fixed pcs errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,10 @@ from models.base import Base
 class Rectangle(Base):
     """ creating rectangle class """
 
-
     @property
     def width(self):
         return self.__width
-    
+
     @property
     def height(self):
         return self.__height
@@ -40,12 +39,9 @@ class Rectangle(Base):
     def y(self, value):
         self.__y = value
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        
-
