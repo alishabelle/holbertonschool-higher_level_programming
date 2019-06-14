796d9d097380e882a64908e893387c5af3222c89
Trill Ali <alishabelle98@gmail.com>
2019-06-14 04:20:57 +0000

    push answer file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" write first class base """
+
+
+class Base:
+    """ creating base class """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """  instantiation of id """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
