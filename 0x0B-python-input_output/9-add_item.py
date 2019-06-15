7108a2c77bedc67bd2f51c762cd2e47b3fc26cb2
Trill Ali <alishabelle98@gmail.com>
2019-06-15 20:59:00 +0000

    push answer file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+
+import json
+import os.path
+import sys
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+that_cheddar = []
+that_cheddar += sys.argv
+new_jawn = []
+if os.path.exists('add_item.json'):
+    new_jawn.extend(load_from_json_file('add_item.json'))
+else:
+    save_to_json_file(new_jawn, 'add_item.json')
+if len(that_cheddar) > 1:
+    new_jawn.extend(that_cheddar[1:])
+save_to_json_file(new_jawn, 'add_item.json')
