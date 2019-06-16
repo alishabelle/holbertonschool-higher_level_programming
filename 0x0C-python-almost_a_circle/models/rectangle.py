55a24e0a830cbeab896b6ab847644e7ee837aa95
Trill Ali <alishabelle98@gmail.com>
2019-06-16 14:58:38 +0000

    push added rect content

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,13 @@ from models.base import Base
 class Rectangle(Base):
     """ creating rectangle class """
 
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
     @property
     def width(self):
         return self.__width
@@ -25,36 +32,32 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
-    if not isinstance(width, int):
-        raise TypeError("width must be an integer")
-    if width <= 0:
-        raise ValueError("width must be > 0")
 
     @height.setter
     def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
-    if not isinstance(height, int):
-        raise TypeError("height must be an integer")
-    if height <= 0:
-        raise ValueError("height must be > 0")
 
     @x.setter
     def x(self, value):
-        self.__x = value
-        if x < 0:
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
             raise ValueError("x must be >= 0")
+        self.__x = value
 
     @y.setter
     def y(self, value):
-        self.__y = value
-        if y < 0:
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
             raise ValueError("y must be >= 0")
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
+        self.__y = value
