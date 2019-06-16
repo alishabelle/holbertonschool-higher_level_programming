6aaa8580fb571372e36f2e0898d8c8395878e49f
Trill Ali <alishabelle98@gmail.com>
2019-06-16 03:50:25 +0000

    push answer file

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        a = self.__dict__
+        if attrs is None:
+            return(a)
+        new_jawn = {}
+        for k, val, in a.items():
+            if k in attrs:
+                new_jawn[k] = val
+        return(new_jawn)
