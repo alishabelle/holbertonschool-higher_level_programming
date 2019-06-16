79ca8c73dc06bed08ac0975b6aa08040a4002ab4
Trill Ali <alishabelle98@gmail.com>
2019-06-16 19:23:15 +0000

    adding parens ()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,6 +67,6 @@ class Rectangle(Base):
         return (self.__width * self.__height)
 
     def __str__(self):
-        return ("[Rectangle] {} {}/{} - {}/{}".
+        return ("[Rectangle] ({}) {}/{} - {}/{}".
                 format(self.id, self.__x, self.__y,
                        self.__width, self.__height))
