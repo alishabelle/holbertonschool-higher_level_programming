b11d77d3c3a9318b19a97ee81a9aaee158ef061d
alishabelle <alishabelle@gmail.com>
2019-07-18 03:48:19 +0000

    push answer file for 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- lists all shows w 1 linked genre
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
