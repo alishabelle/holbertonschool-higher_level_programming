33d6a32b459d83eb5ab7106003795bb6121a7196
alishabelle <alishabelle@gmail.com>
2019-07-18 02:14:14 +0000

    push files 4 & 5

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- script that creates a table id_not_full
+CREATE table IF NOT EXISTS id_not_null (id INT default 1, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- script that creates a table unique_id
+CREATE table IF NOT EXISTS unique_id (id INT default 1 UNIQUE, name VARCHAR(256));
