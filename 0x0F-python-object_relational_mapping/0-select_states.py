2f37155840c57f274d18fca96dcc8372cc0d4341
alishabelle <alishabelle98@gmail.com>
2019-08-10 21:13:44 +0000

    push new file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" lists states in a database """
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd="dub",
+        db=sys.argv[3]
+    )
+
+
+    cur = db.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cur.execute(query)
+    records = cur.fetchall()
+
+
+    for record in records:
+        print(record)
+
+    cur.close()
+    db.close()
