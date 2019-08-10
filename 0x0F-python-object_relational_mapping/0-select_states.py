a023338b6752e86314010540889e016699003587
alishabelle <alishabelle98@gmail.com>
2019-08-10 17:46:02 +0000

    pycodestyle error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,11 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect (
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
-        passwd="dub",
+        passwd=sys.argv[2],
         db=sys.argv[3],
     )
 
