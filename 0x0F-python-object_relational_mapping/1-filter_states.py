15a73b37a88f5ae6dba43a34ab3fe1cd9abc6c8b
alishabelle <alishabelle98@gmail.com>
2019-08-10 18:49:41 +0000

    push answer file for checks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
         host="localhost",
         port=3306,
         user=sys.argv[1],
-        passwd=sys.argv[2],
+        passwd="dub",
         db=sys.argv[3],
     )
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'N%'"
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     cur.execute(query)
     records = cur.fetchall()
 
