e36dc4607ba6d3ca66e361ddcf20dd92bf1ce291
alishabelle <alishabelle98@gmail.com>
2019-08-11 16:59:56 +0000

    push answer file to 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" script from #2 safe of sql injection """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cur = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name FROM cities \
+INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC"
+    cur.execute(query)
+    records = cur.fetchall()
+
+    for record in records:
+        print(record)
+
+    cur.close()
+    db.close()
