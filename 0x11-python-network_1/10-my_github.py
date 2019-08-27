6ba70655e2d69f764ba0c4b633f740f9ef389dd6
alishabelle <alishabelle98@gmail.com>
2019-08-27 03:58:29 +0000

    change pass to passwd

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,8 +6,8 @@ import sys
 
 if __name__ == '__main__':
     usr = sys.argv[1]
-    pass = sys.argv[2]
+    passwd = sys.argv[2]
     url = 'https://api.github.com/users/{}'.format(sys.argv[1])
-    aye = requests.get(url, auth=(usr, pass))
+    aye = requests.get(url, auth=(usr, passwd))
     aye = dict(aye.json())
     print(aye.get('id'))
