50c615460df8869fd16bb43a61a1f58fe1c3c32a
alishabelle <alishabelle98@gmail.com>
2019-08-27 03:50:23 +0000

    fix output

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,16 +6,16 @@ import requests
 
 if __name__ == '__main__':
 
-url = 'http://0.0.0.0:5000/search_user'
-try:
-    aye = requests.post(url, data={'q': sys.argv[1]})
-except IndexError:
-    aye = requests.post(url, data={'q': ""})
-try:
-    aye = aye.json()
-    if not aye:
-        print('No result')
-    else:
-        print('[{}] {}'.format(aye.get('id'), aye.get('name')))
-except ValueError:
-    print('Not a valid JSON')
+    url = 'http://0.0.0.0:5000/search_user'
+    try:
+        aye = requests.post(url, data={'q': sys.argv[1]})
+    except IndexError:
+        aye = requests.post(url, data={'q': ""})
+    try:
+        aye = aye.json()
+        if not aye:
+            print('No result')
+        else:
+            print('[{}] {}'.format(aye.get('id'), aye.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
