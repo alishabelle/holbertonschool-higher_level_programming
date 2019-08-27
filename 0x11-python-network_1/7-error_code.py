dee95c584a18ec6cfb9a82094bd9d57f3b818894
alishabelle <alishabelle98@gmail.com>
2019-08-27 03:38:27 +0000

    fixed underscore

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,9 +7,10 @@ import requests
 
 if __name__ == '__main__':
 
+    
     set = requests.get(sys.argv[1])
 
-    if set.status.code >= 400:
+    if set.status_code >= 400:
         print("Error code: {}".format(ret.status_code))
     else:
         print(set.text)
