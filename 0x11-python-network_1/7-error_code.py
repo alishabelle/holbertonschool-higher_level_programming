011c0c0e03d4ac1547bd486c8252717026d223e0
alishabelle <alishabelle98@gmail.com>
2019-08-27 03:35:38 +0000

    push answer file to 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" takes url sends request displays responese """
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+
+    set = requests.get(sys.argv[1])
+
+    if set.status.code >= 400:
+        print("Error code: {}".format(ret.status_code))
+    else:
+        print(set.text)
