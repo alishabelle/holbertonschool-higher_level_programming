b3bfb4264e2b7b5a55dd3d00536c749d87ab7c4c
alishabelle <alishabelle98@gmail.com>
2019-09-17 21:08:02 +0000

    fixed spelling error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
+
 if (process.argv.slice(2).length === 0) {
-  console.log('No arguement');
-} else if (process.argv.slice(2).length < 2) {
+  console.log('No argument');
+} else if (process.argv.slice(2).length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
