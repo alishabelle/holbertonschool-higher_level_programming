dea0b575c37777455e0d269ce09630cdbdf631a9
alishabelle <alishabelle98@gmail.com>
2019-09-17 21:45:08 +0000

    push answer file to 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arrayLen = array.length;
+
+for (let a = 0; a < arrayLen; a++) {
+  console.log(array[a]);
+}
