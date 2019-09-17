a251255636fa335dc5c0b8d2c4d58dfbafb4dea1
alishabelle <alishabelle98@gmail.com>
2019-09-17 21:06:23 +0000

    push new output answer

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-/*console.log(process.argv);
-console.log(process.argv.slice(2).length);*/
 if (process.argv.slice(2).length === 0) {
   console.log('No arguement');
-} else if (process.argv.slice(2).length === 1) {
+} else if (process.argv.slice(2).length < 2) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
