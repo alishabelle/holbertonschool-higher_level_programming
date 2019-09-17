95c368b2cb43650acd2b49ecdbc2567c9519155f
alishabelle <alishabelle98@gmail.com>
2019-09-17 22:55:02 +0000

    fixed semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-
 if (process.argv[2] === undefined) {
-    console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }
 
 for (let a = 0; a < process.argv[2]; a++) {
-    console.log('C is fun');
+  console.log('C is fun');
 }
