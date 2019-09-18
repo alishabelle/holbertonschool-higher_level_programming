82d3a575f1279a2b63a663c15cb670eb12a5f106
alishabelle <alishabelle98@gmail.com>
2019-09-18 18:07:01 +0000

    added variable to top hope for output check

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
+let array = process.argv;
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let  array = process.argv.sort();
+  array = array.sort();
   const see = array[array.length - 2];
   console.log(parseInt(see));
 }
