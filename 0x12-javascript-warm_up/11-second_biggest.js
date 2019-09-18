7022569218c4b072432a91a381d1b504a31401bd
alishabelle <alishabelle98@gmail.com>
2019-09-18 17:58:19 +0000

    push for new output

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const array = process.argv.sort();
-  const see = array[process.argv.length - 2];
+  let  array = process.argv.sort();
+  const see = array[array.length - 2];
   console.log(parseInt(see));
 }
