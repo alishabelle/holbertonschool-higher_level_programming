3d5fee89506946267193f75e17e3e88db72e72e7
alishabelle <alishabelle98@gmail.com>
2019-09-24 01:25:59 +0000

    push answer file to advance 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const f1 = fs.readFileSync(process.argv[2]);
+const f2 = fs.readFileSync(process.argv[3]);
+fs.appendFile(process.argv[4], f1, (err) => {
+    if (err) throw err;
+});
+fs.appendFile(process.argv[4], f2, (err) => {
+    if (err) throw err;
+});
+
