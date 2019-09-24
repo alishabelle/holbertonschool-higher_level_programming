5b95d91d525d07886156ec2b1f5d127fb97f6d30
alishabelle <alishabelle98@gmail.com>
2019-09-24 23:09:12 +0000

    fixed semistandard errors

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,8 +2,8 @@
 
 const request = require('request');
 request(process.argv[2], function (error, response) {
-    if (error) {
-	console.log(error);
-    }
-    console.log('code:', response.statusCode);
+  if (error) {
+    console.log(error);
+  }
+  console.log('code:', response.statusCode);
 });
