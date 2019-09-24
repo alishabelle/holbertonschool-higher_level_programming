29e0b83bad8bff79a3134ba113ccbfff6b36730f
alishabelle <alishabelle98@gmail.com>
2019-09-24 01:27:04 +0000

    fixed semistandard errors

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,9 +5,8 @@ const fs = require('fs');
 const f1 = fs.readFileSync(process.argv[2]);
 const f2 = fs.readFileSync(process.argv[3]);
 fs.appendFile(process.argv[4], f1, (err) => {
-    if (err) throw err;
+  if (err) throw err;
 });
 fs.appendFile(process.argv[4], f2, (err) => {
-    if (err) throw err;
+  if (err) throw err;
 });
-
