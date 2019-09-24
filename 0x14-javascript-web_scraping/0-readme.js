dd26c1625e9169883e11608ad434196e299ea07b
alishabelle <alishabelle98@gmail.com>
2019-09-24 22:40:56 +0000

    fixed semistandard errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
