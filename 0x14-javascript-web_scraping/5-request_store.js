b4b39d65b6dd6f0989c1bb779ab6802da2f41506
alishabelle <alishabelle98@gmail.com>
2019-09-25 00:42:16 +0000

    switched parameters to produce output

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,12 +6,12 @@ const url = process.argv[2];
 const fp = process.argv[3];
 
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  fs.writeFile(fp, 'utf8', body, (error) => {
     if (error) {
-      console.log(error);
+	console.log(error);
     }
-  });
+    fs.writeFile(fp, body, 'utf8', (error) => {
+	if (error) {
+            console.log(error);
+	}
+    });
 });
