64c617db6bb759ce0f8d79f345e2d8514ea4b662
alishabelle <alishabelle98@gmail.com>
2019-09-25 00:39:51 +0000

    pushing answer file to 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const fp = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(fp, 'utf8', body, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
